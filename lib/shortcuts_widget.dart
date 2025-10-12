fdca80d2d6dc7d549bc92372cc84b5a1773d64c2
Squizzy <crazy_squirrel@yahoo.com>
2025-10-12 16:45:19 +0100

    Widget to handle shortcuts

---
+++ lib/shortcuts_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FnKeys extends StatelessWidget {
+  const FnKeys({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("Shortcuts")],
+      ),
+    );
+  }
+}
