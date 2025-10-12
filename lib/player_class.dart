ffba3f4bfeeb55294be78f9216732950af17a09a
Squizzy <crazy_squirrel@yahoo.com>
2025-10-12 16:41:39 +0100

    File containing the class to instantiate a player

---
+++ lib/player_class.dart
@@ -0,0 +1,22 @@
+class PlayerInfo {
+  bool iD = false;
+  String name = "";
+  int startValue = 0;
+  List<int> scored = [];
+  List<int> toGo = [];
+  Map<String, int> stats = {};
+
+  PlayerInfo({required this.iD, required this.name, required this.startValue}) {
+    toGo.add(startValue);
+    initStats();
+  }
+
+  void initStats() {
+    stats = {"Start Value": startValue, "Rounds launched": scored.length};
+  }
+
+  int remainder() {
+    return toGo[toGo.length - 1];
+  }
+}
+
