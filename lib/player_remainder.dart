fa75c8a3b160d5501da09daabb2bc04bf9ca81a9
Squizzy <crazy_squirrel@yahoo.com>
2025-10-20 14:28:38 +0100

    Improved the calling by passing the player rather than the remainder; added logging;

--- lib/player_remainder.dart
+++ lib/player_remainder.dart
@@ -1,25 +1,32 @@
+import 'package:first_app/player_class.dart';
 import 'package:flutter/material.dart';
 
-class PlayerRemainder extends StatelessWidget {
-  const PlayerRemainder({super.key, required this.remainder});
+import 'package:logging/logging.dart';
 
-  final int remainder;
+final Logger log = Logger("player_remainder_widget");
+
+class PlayerRemainder extends StatelessWidget {
+  const PlayerRemainder({super.key, required this.player});
+
+  final PlayerInfo player;
 
   @override
   Widget build(BuildContext context) {
+    log.finer("Preparing ${player.name} remainder");
+
     final theme = Theme.of(context);
 
     return Container(
       width: double.infinity,
       padding: EdgeInsets.all(0.0),
-      child: DecoratedBox (
+      child: DecoratedBox(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: theme.colorScheme.inverseSurface,
         ),
-      
+
         child: Text(
-          "$remainder",
+          "${player.remainder()}",
           style: TextStyle(fontSize: 40, color: theme.colorScheme.surface),
           textAlign: TextAlign.center,
         ),
